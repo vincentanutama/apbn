f4784427e127188620ade3fe9648c3880ebe3d74
Vincent Tanutama <Vincent.tanutama@gmail.com>
2020-10-07 14:47:49 -0500

    Added files from DJPK blog

---
+++ 01_Data/01_Raw/djpkblog/README.txt
@@ -0,0 +1,25 @@
+Filename convention
+
+YYYY_A1_V1_YYMMDD
+
+Components:
+
+1. YYYY = Tahun Anggaran
+
+2. A/R  = Anggaran/Realisasi
+
+3.
+1 Ringkasan
+2 Belanja per Fungsi
+3 Pendapatan per jenis pajak
+4 Pendapatan per jenis retribusi
+5 Semester 1
+6 Neraca
+
+4. version
+
+5. Date Update
+
+Usually only A/R1 (Ringkasan) has multiple updates
+
+DATA SOURCE: http://www.djpk.kemenkeu.go.id/?p=5412
--- 01_Data/05_Code/combine-djpk.R
+++ 01_Data/05_Code/combine-djpk.R
@@ -1,3 +1,46 @@
 
   folder <- c('01_Data/02_Temp/apbd')
+  files  <- list.files(folder)
+  pcode  <- as.numeric(substr(files, 1, 2))
+  rds    <- file.path(folder, files)
+  data   <- lapply(rds, readRDS)
+  provcode <- read.csv('01_Data/01_Raw/provcode.csv')
+  bpscode  <- provcode$bpscode[pcode]
+  provname <- provcode$prov[pcode]
+  data     <- Map(function(x, y, z) {
+    x$Prov <- y
+    x$bpscode <- z
+    return(x)
+  }, data, as.list(provname), as.list(bpscode))
+  cname    <- colnames(data[[1]])
+  data     <- lapply(data, function(x) {
+    colnames(x) <- cname
+    return(x)
+  })
+  data     <- do.call('rbind', data)
+  
+  # Clean numbers
+  data <- data[-which(is.na(data$Akun)),]
+  connum <- function(x)  as.numeric(gsub('\\,', '.', 
+                                    gsub('\\.|\\-', '', x)))
+  data$Anggaran  <- connum(data$Anggaran)
+  data$Realisasi <- connum(data$Realisasi)
+  data$Persen    <- data$Realisasi/data$Anggaran * 100
+  data$Persen[which(!is.finite(data$Persen))] <- NA
+  data$`%` <- NULL
+  
+  # Add categories
+  code <- read.csv('01_Data/01_Raw/djpkcode.csv')
+  # merge now
+  
+  # Combine with BPS kabupatne code
+  # combine provcode
+  
+  # Check sums
+  
+  
+  
+  # Summary statistics
+  
+  
   
